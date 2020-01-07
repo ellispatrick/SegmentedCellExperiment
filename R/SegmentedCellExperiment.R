22baa47d27a3e3115e6ce887cc69716366d7f280
Ellis Patrick <epatrick87@gmail.com>
2020-01-07 20:05:02 +1100

    Added basic manual

--- R/SegmentedCellExperiment.R
+++ R/SegmentedCellExperiment.R
@@ -1,3 +1,41 @@
+#' The SegmentedCellExperiment class
+#'
+#' @param cellData A data frame that contains at least the columns [x] and [y] giving the location of each cell.
+#' @param cellProfiler A logical indicating that [cellData] is in a format similar to what cellProfiler outputs.
+#' @param spatialCoords The column names corresponding to spatial coordinates. eg. x, y, z...
+#' @param cellTypeString The name of the column that contains cell type calls.
+#' @param intensityString A string which can be used to identify the columns which contain marker intensities. (This needs to be extended to take the column names themselves.)
+#' @param morphologyString A string which can be used to identify the columns which contains morphology information.
+#' 
+#' @examples
+#' ### Something that resembles cellProfiler data
+#'
+#' set.seed(51773)
+#'
+#' n = 10
+#'
+#' cells <- data.frame(row.names = seq_len(n))
+#' cells$ObjectNumber <- seq_len(n)
+#' cells$ImageNumber <- rep(1:2,c(n/2,n/2))
+#' cells$x <- runif(n)
+#' cells$y <- runif(n)
+#' cells$AreaShape_round <- rexp(10)
+#' cells$AreaShape_diameter <- rexp(10, 2)
+#' cells$Intensity_Mean_CD8 <- rexp(10, 10)
+#' cells$Intensity_Mean_CD4 <- rexp(10, 10)
+#'
+#' cellExp <- SegmentedCellExperiment(cells, cellProfiler = TRUE)
+#' 
+#' ### Cluster cell types
+#' intensities <- intensity(cellExp)
+#' kM <- kmeans(intensities,2)
+#' cellType(cellExp) <- paste('cluster',kM$cluster, sep = '')
+#' location(cellExp)
+#' 
+#' @export
+#' @rdname SegmentedCellExperiment
+#' @importFrom methods new
+#' @importClassesFrom S4Vectors DataFrame
 
 SegmentedCellExperiment <- function(cellData, cellProfiler = FALSE, spatialCoords = c('x','y'), cellTypeString = NULL, intensityString = NULL, morphologyString = NULL) {
 
